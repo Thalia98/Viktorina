08d19ccb487dd3caccdac75ff64b4a9fab6c8651
Thalía Gutiérrez Suárez <thalia.gutierrez@aitana.es>
2021-06-07 21:15:56 +0200

    Add loading and sig in Login

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FirebaseManagementService } from 'src/app/services/firebase-management.service';
 import { ERROR_FORM } from '../../../globalValues';
 @Component({
   selector: 'app-login',
@@ -9,18 +13,37 @@ import { ERROR_FORM } from '../../../globalValues';
 export class LoginComponent implements OnInit {
   formGroup: FormGroup;
   errorFormText = ERROR_FORM;
+  loading: boolean = false;
 
-  constructor(public formBuilder: FormBuilder) {
+  constructor(
+    public formBuilder: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private firebaseManagementService: FirebaseManagementService,
+    private router: Router,
+  ) {
     this.formGroup = this.formBuilder.group({
       user: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
-   }
+  }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   login() {
-    console.log(this.formGroup);
+    this.loading = true;
+
+    this.afAuth.signInWithEmailAndPassword(this.formGroup.get('user').value, this.formGroup.get('password').value).then(res => {
+      if(res.user?.emailVerified){
+
+      } else {
+        this.router.navigate(['/user/verify']);
+      }
+    }).catch(error => {
+      this.loading = false;
+      this.formGroup.reset();
+      this.toastr.error(this.firebaseManagementService.errorsFirebase(error.code), 'Oops ha ocurrido un error');
+    });
   }
 
 }
