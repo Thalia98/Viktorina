9c10869a4b4b57d1b84e5a63ce1b5f5eddcda081
Thalía Gutiérrez Suárez <thalia.gutierrez@aitana.es>
2021-06-07 21:18:56 +0200

    Add recover password function

--- src/app/components/user/recover-password/recover-password.component.ts
+++ src/app/components/user/recover-password/recover-password.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { ERROR_FORM } from 'src/app/globalValues';
+import { FirebaseManagementService } from 'src/app/services/firebase-management.service';
 
 @Component({
   selector: 'app-recover-password',
@@ -10,8 +14,15 @@ import { ERROR_FORM } from 'src/app/globalValues';
 export class RecoverPasswordComponent implements OnInit {
   formGroup: FormGroup;
   errorFormText = ERROR_FORM;
+  loading: boolean = false;
 
-  constructor(public formBuilder: FormBuilder) {
+  constructor(
+    public formBuilder: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private firebaseManagementService: FirebaseManagementService,
+    private router: Router,
+    ) {
     this.formGroup = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]]
     });
@@ -20,7 +31,18 @@ export class RecoverPasswordComponent implements OnInit {
   ngOnInit() { }
 
   recoverPass() {
-    console.log(this.formGroup);
+    this.loading = true;
+
+    this.afAuth.sendPasswordResetEmail(this.formGroup.get('email').value).then(() => {
+      this.toastr.info('Restablecer contraseña', 'Enviamos un correo para restablecer la contraseña.')
+      this.router.navigate(['/user']);
+    }).catch( error => {
+      this.loading = false;
+
+      this.toastr.error(this.firebaseManagementService.errorsFirebase(error.code));
+      this.formGroup.reset();
+    });
+    
   }
 
 }
